58da534f44d17cfa0feecb27aed0c60f68241eb7
chakradhar0622 <chakradharguravana25@gmail.com>
2025-07-07 14:34:05 +0530

    Fix: Add CORS to backend

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
